b02aee7c21436028a37f2bcb493d24466a0ee0c8
Milan Patel <milan888@yahoo.com>
2025-07-08 21:01:05 -0400

    Create CREATE TABLE sales_order.sql

---
+++ src/CREATE TABLE sales_order.sql
@@ -0,0 +1,12 @@
+CREATE TABLE public.sales_order (
+    order_id text,
+    order_type text,
+    description text,
+    product_type_name text,
+    customer_id text,
+    quantity text,
+    order_date text,
+    update_datetime text,
+    updated_by_user text,
+    updated_by_server text
+);
